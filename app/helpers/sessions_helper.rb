3f8f48f13ed7892cadeb2a9ea2b99a214d468c9c
masanag <masaki.nagaishi@gmail.com>
2016-03-16 09:00:23 +0900

    Done 8.2.4

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,14 @@
 #
 module SessionsHelper
   def sign_in(user)
-    remenber_token = User.new_remember_token
-    cookies.permanent[:remenber_token] = remenber_token
-    user.update_attribute(:remenber_token, User.encrypt(remenber_token))
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
     self.current_user = user
+  end
+
+  def signed_in?
+    !current_user.nil?
   end
 
   def current_user=(user)
@@ -12,7 +16,7 @@ module SessionsHelper
   end
 
   def current_user
-    remenber_token = User.encrypt(cookies[:remenber_token])
-    @current_user ||= User.find_by(remember_token: remenber_token)
+    remember_token = User.encrypt(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
   end
 end
