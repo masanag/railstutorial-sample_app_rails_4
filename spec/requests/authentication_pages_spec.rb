d32b0a3dac44341fd8e7573e7468744fd691f6b6
masanag <masaki.nagaishi@gmail.com>
2016-03-18 07:41:47 +0900

    before will_paginate

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -23,9 +23,10 @@ describe 'Authentication' do
 
     describe 'with valid information' do
       let(:user) { FactoryGirl.create(:user) }
-      before { valid_signin(user) }
+      before { sign_in user }
 
       it { should have_title(user.name) }
+      it { should have_link('Users', href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
@@ -65,6 +66,11 @@ describe 'Authentication' do
         describe 'submitting to the update action' do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe 'visiting the user index' do
+          before { visit users_path }
+          it { should have_title('Sign in') }
         end
       end
     end
