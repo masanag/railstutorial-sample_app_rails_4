2ae81c974c70ce0063e2ea114bb8a8f1031ef176
masanag <masaki.nagaishi@gmail.com>
2016-03-18 07:41:47 +0900

    before will_paginate

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -13,12 +13,6 @@ def sign_in(user, options = {})
   end
 end
 
-def valid_signin(user)
-  fill_in 'Email', with: user.email
-  fill_in 'Password', with: user.password
-  click_button 'Sign in'
-end
-
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     expect(page).to have_selector('div.alert.alert-error', text: message)
