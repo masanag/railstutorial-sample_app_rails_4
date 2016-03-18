79b0fcca37b946f64899bbdcbaaaada32e77a85a
masanag <masaki.nagaishi@gmail.com>
2016-03-18 09:01:42 +0900

    Finish user edit, update, index and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,14 @@ describe User do
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+  end
 
   describe 'when name is not present' do
     before { @user.name = '' }
