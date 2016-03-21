70b4ac0340fc7403ac274137f16fb98fbfcfd336
masanag <masaki.nagaishi@gmail.com>
2016-03-21 22:25:36 +0900

    done 10.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 #
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   before_create :create_remember_token
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
