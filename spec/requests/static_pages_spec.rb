659e91e78c46c82ff43287aadbd9f5ff647cb26c
masanag <masaki.nagaishi@gmail.com>
2016-03-22 17:34:10 +0900

    done excercise 10.5.2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -29,6 +29,28 @@ describe 'Static pages' do
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
       end
+
+      describe 'pagination' do
+        before do
+          40.times { create(:micropost, user: user) }
+          visit root_path
+        end
+        after { Micropost.delete_all }
+        it { should have_selector('div.pagination') }
+        it 'should list each micropost' do
+          user.microposts.paginate(page: 1).each do |micropost|
+            expect(page).to have_selector('li', text: micropost.content)
+          end
+        end
+        context 'access page=2' do
+          before { visit root_path(page: 2) }
+          it 'should list each micropost' do
+            user.microposts.paginate(page: 2).each do |micropost|
+              expect(page).to have_selector('li', text: micropost.content)
+            end
+          end
+        end
+      end
     end
   end
 
