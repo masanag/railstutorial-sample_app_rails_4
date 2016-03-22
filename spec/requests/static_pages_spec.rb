f737f9dcb247a1499e204941b0bf44fc81615058
masanag <masaki.nagaishi@gmail.com>
2016-03-22 13:41:55 +0900

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,6 +14,22 @@ describe 'Static pages' do
 
     it_behaves_like 'all static pages'
     it { should_not have_title('| Home') }
+
+    describe 'for signed-in users' do
+      let(:user) { create(:user) }
+      before do
+        create(:micropost, user: user, content: 'Lorem ipsum')
+        create(:micropost, user: user, content: 'Dolor sit amet')
+        sign_in user
+        visit root_path
+      end
+
+      it 'should render the user\'s feed' do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
   end
 
   describe 'Help page' do
